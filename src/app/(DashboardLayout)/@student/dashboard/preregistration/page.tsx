0844e6494231a052f9ace996690c6c5160ae7af2
almamun528 <ayapths@gmail.com>
2026-05-02 01:23:54 +0600

    I have select the preRegistration page

--- src/app/(DashboardLayout)/@student/dashboard/preregistration/page.tsx
+++ src/app/(DashboardLayout)/@student/dashboard/preregistration/page.tsx
@@ -16,6 +16,9 @@ const Preregistration = async () => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Preregistration</h1>
+      <h1 className="text-red-500 text-3xl text-center">
+        I Am Mamun I have taken pre-Regristration page.. dont select{" "}
+      </h1>
       <div className="bg-white/5 border border-white/10 rounded-xl p-6 backdrop-blur-sm">
         <p className="text-gray-400">
           Complete your course preregistration for the upcoming semester.
