3ad34311b890c62c44b40a071919af38f53f7aa5
umme-Tahazzee <tahazzeekaria1@gmail.com>
2026-05-02 19:44:03 +0600

    feat: implement AdminCard component for exam payment status and file download

--- src/app/(DashboardLayout)/@student/dashboard/admit-card/page.tsx
+++ src/app/(DashboardLayout)/@student/dashboard/admit-card/page.tsx
@@ -1,16 +1,8 @@
+import AdmitCardPage from "@/Components/AdmitCard/AdmitCard";
 const AdmitCard = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Admit Card </h1>
-      <h2 className="text-red-600 text-7xl">
-        I choose this page for developing more . Name: Umme Tahaazzee
-      </h2>
-
-      <div className="bg-white/5 border border-white/10 rounded-xl p-6 backdrop-blur-sm">
-        <p className="text-gray-400">
-          Download your examination admit cards here.
-        </p>
-      </div>
+      <AdmitCardPage />
     </div>
   );
 };
