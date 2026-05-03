3d10c08f55619f1addfeafac01577331f74b8bd6
rasel <rasel708211@gmail.com>
2026-05-03 11:22:15 +0600

    feat: implement ResultChart and CreateChart for student performance visualization

--- src/app/(DashboardLayout)/@student/dashboard/grades/page.tsx
+++ src/app/(DashboardLayout)/@student/dashboard/grades/page.tsx
@@ -1,8 +1,11 @@
 import GradeChart from "./../../../../../Components/GradeChart/GradeChart";
+import ResultChart from "./../../../../../Components/ResultChart/ResultChart";
+
 const Grades = () => {
   return (
     <div>
       <GradeChart />
+      <ResultChart />
     </div>
   );
 };
